7b27714a19cd050f6f4a93b0e55e5b80ca9708ac
Amer Jusupovic <amerjusupovic00@gmail.com>
2021-12-13 16:09:39 -0600

    fix sidebar, add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,26 @@
 import React from 'react';
 import './App.scss';
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import { Menu } from 'antd';
 
 function App() {
   return (
-    <div className="start">
-      <div className="sidebar-div">
-        <ProSidebar width="100%" className="sidebar">
-          <Menu iconShape="square">
-            <MenuItem>Dashboard</MenuItem>
-            <SubMenu title="Components">
-              <MenuItem>Component 1</MenuItem>
-              <MenuItem>Component 2</MenuItem>
-            </SubMenu>
+    <Router>
+      <div className="start">
+        <div className="sidebar-div">
+          <Menu>
+            <Menu.Item>Menu</Menu.Item>
+            <Menu.SubMenu title="SubMenu">
+              <Menu.Item>SubMenuItem</Menu.Item>
+            </Menu.SubMenu>
           </Menu>
-        </ProSidebar>
-      </div>
-      <div className="main">
-        <div className="center-left">
-          <div className="container">
-            <div className="h title letter">H</div>
-            <div className="e title letter">E</div>
-            <div className="y title letter">Y</div>
-            <div className="ex title letter">!</div>
-          </div>
-          <div className="container">
-            <div className="si subtitle letter">I</div>
-            <div className="sap subtitle letter">'</div>
-            <div className="sm subtitle letter">M&nbsp;</div>
-            <div className="sa subtitle letter">A</div>
-            <div className="sm subtitle letter">M</div>
-            <div className="se subtitle letter">E</div>
-            <div className="sr subtitle letter">R</div>
-          </div>
         </div>
       </div>
-    </div>
+      <Routes>
+        <Route path="/" element = { <Home/> }/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/Home.tsx
@@ -0,0 +1,29 @@
+import "./App.scss";
+
+function Home() {
+    return (
+      <div className="start">
+        <div className="main">
+          <div className="center-left">
+            <div className="container">
+              <div className="h title letter">H</div>
+              <div className="e title letter">E</div>
+              <div className="y title letter">Y</div>
+              <div className="ex title letter">!</div>
+            </div>
+            <div className="container">
+              <div className="si subtitle letter">I</div>
+              <div className="sap subtitle letter">'</div>
+              <div className="sm subtitle letter">M&nbsp;</div>
+              <div className="sa subtitle letter">A</div>
+              <div className="sm subtitle letter">M</div>
+              <div className="se subtitle letter">E</div>
+              <div className="sr subtitle letter">R</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Home;
