d89dbe751315e89c4b8ef498ad032e5d8d0da51a
Amer Jusupovic <amerjusupovic00@gmail.com>
2021-12-19 11:10:30 -0800

    add icons, collapse state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import { Menu } from 'antd';
+import { GithubOutlined, GitlabOutlined } from '@ant-design/icons';
 
 function App() {
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
     <Router>
       <div className="start sticky">
         <div className="sidebar-div sticky">
-          <Menu mode={"inline"} theme={"dark"}>
+          <Menu mode={"inline"} theme={"dark"} inlineCollapsed={collapsed}>
             <Menu.Item>Menu</Menu.Item>
             <Menu.SubMenu title="SubMenu">
               <Menu.Item>SubMenuItem</Menu.Item>
             </Menu.SubMenu>
+            <Menu.Item icon={<GitlabOutlined/>}/>
           </Menu>
         </div>
       </div>
